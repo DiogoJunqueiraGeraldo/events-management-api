55fa6bc1b130bde6c1dbb48d037be42263048b68
Diogo Geraldo <diogojunqueirageraldo@gmail.com>
2025-04-01 22:39:01 -0300

    Add: project structure

---
+++ src/main/java/com/isiflix/events_management_api/EventsManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.isiflix.events_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isiflix/events_management_api/EventsManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isiflix.events_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
