5ba39f51d3750b68277082bff7f0d202c5e3a305
Diogo Geraldo <diogojunqueirageraldo@gmail.com>
2025-04-11 13:56:27 -0300

    Fix: users table sequence allocation size

--- src/main/resources/db/sql/V2__create_users_table.sql
+++ src/main/resources/db/sql/V2__create_users_table.sql
@@ -1,9 +1,17 @@
+create sequence ems_users_id_seq
+    start with 1
+    increment by 50
+    minvalue 1
+    no maxvalue
+    cache 1;
+
 create table if not exists ems_users
 (
-    id               bigserial primary key,
+    id               bigint primary key,
     name             varchar(200) not null,
     email            varchar(200) not null unique,
     created_datetime timestamp default now()
 );
 
 -- drop table ems_users;
+-- drop sequence ems_users_id_seq;
